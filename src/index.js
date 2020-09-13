0d77c5838537ef79413833253ba9fa2ba033697b
encomp <edgar.martinez.rico@gmail.com>
2020-09-12 20:27:17 -0700

    Set up Material-UI

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import Auth from './layouts/Auth';
 import theme from './theme';
 import * as firebase from 'firebase';
 
@@ -12,7 +13,7 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <Auth />
   </ThemeProvider>,
   document.querySelector('#root'),
 );
