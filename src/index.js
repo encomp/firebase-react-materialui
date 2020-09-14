0ca459ecf28a4f9c6eac24739a09d269e950587e
encomp <edgar.martinez.rico@gmail.com>
2020-09-14 14:17:58 -0700

    Fix index

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
-import Auth from './layouts/Auth';
 import theme from './theme';
-import * as firebase from 'firebase';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <Auth />
+    <App />
   </ThemeProvider>,
   document.querySelector('#root'),
 );
